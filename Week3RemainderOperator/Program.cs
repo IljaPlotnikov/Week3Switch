506d6c7291bb05cbc4156473c692dd332b0cc9bb
Ilja Plotnikov <example@mail.com>
2024-01-14 23:50:56 +0200

    week 3

---
+++ Week3RemainderOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿//Odd or even
+
+int a = 11;
+
+int result = a % 2;
+
+Console.WriteLine(result);
+
+if (result == 0)
+{
+    Console.WriteLine($"{a} is an even number");
+else
+{
+    Console.Write($"{a} is an odd number");
+}
